a661f9462a386ad0677a30b2671d26f395721159
xelphin <111276630+xelphin@users.noreply.github.com>
2024-03-24 19:44:06 +0200

    Init

---
+++ mdp_implementation.py
@@ -0,0 +1,79 @@
+from copy import deepcopy
+import numpy as np
+
+
+def value_iteration(mdp, U_init, epsilon=10 ** (-3)):
+    # TODO:
+    # Given the mdp, the initial utility of each state - U_init,
+    #   and the upper limit - epsilon.
+    # run the value iteration algorithm and
+    # return: the U obtained at the end of the algorithms' run.
+    #
+
+    # ====== YOUR CODE: ======
+    raise NotImplementedError
+    # ========================
+
+
+def get_policy(mdp, U):
+    # TODO:
+    # Given the mdp and the utility of each state - U (which satisfies the Belman equation)
+    # return: the policy
+    #
+
+    # ====== YOUR CODE: ======
+    raise NotImplementedError
+    # ========================
+
+
+def policy_evaluation(mdp, policy):
+    # TODO:
+    # Given the mdp, and a policy
+    # return: the utility U(s) of each state s
+    #
+
+    # ====== YOUR CODE: ======
+    raise NotImplementedError
+    # ========================
+
+
+def policy_iteration(mdp, policy_init):
+    # TODO:
+    # Given the mdp, and the initial policy - policy_init
+    # run the policy iteration algorithm
+    # return: the optimal policy
+    #
+
+    # ====== YOUR CODE: ======
+    raise NotImplementedError
+    # ========================
+
+
+
+"""For this functions, you can import what ever you want """
+
+
+def get_all_policies(mdp, U):  # You can add more input parameters as needed
+    # TODO:
+    # Given the mdp, and the utility value U (which satisfies the Belman equation)
+    # print / display all the policies that maintain this value
+    # (a visualization must be performed to display all the policies)
+    #
+    # return: the number of different policies
+    #
+
+    # ====== YOUR CODE: ======
+    raise NotImplementedError
+    # ========================
+
+
+def get_policy_for_different_rewards(mdp):  # You can add more input parameters as needed
+    # TODO:
+    # Given the mdp
+    # print / displas the optimal policy as a function of r
+    # (reward values for any non-finite state)
+    #
+
+    # ====== YOUR CODE: ======
+    raise NotImplementedError
+    # ========================
